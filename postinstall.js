ea6afdf4084177fc63e7cd17274640789bc8b640
黄勤坤 <hqk616@163.com>
2024-05-10 09:36:03 +0800

    opt

--- postinstall.js
+++ postinstall.js
@@ -1,10 +1,6 @@
 require('colors');
 const fs = require('fs');
 
-// 创建必要文件
-// 创建 .env 文件
+// Create necessary files
 fs.writeFileSync('.env', '')
-// 创建 config.json 文件
 fs.writeFileSync('config.json', '{}')
-
-console.log('Now you can use surgejs.'.green)
