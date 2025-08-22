4be9c11cf9d5cf433c334af80df544a4e6d6df4d
黄勤坤 <hqk616@163.com>
2025-08-22 17:26:48 +0800

    Clean up useless files

--- middleware/terminals.js
+++ middleware/terminals.js
@@ -1,15 +1,12 @@
 'use strict';
 
 // This is the terminal command collection file
-const Config = require('./commands/config');
 const Deploy = require('./commands/deploy');
 const Server = require('./commands/server');
 const Show = require('./commands/show');
-const Store = require('./commands/store');
 
 module.exports = {
   deploy: Deploy,
-  config: Config,
   server: Server,
   show: Show
 };
